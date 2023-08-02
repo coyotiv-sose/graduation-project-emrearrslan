9f11daefd8220d03791d8264eb8e438ae2432c7b
Emre ARSLAN <me.emrearslan@gmail.com>
2023-08-02 09:56:12 +0200

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 console.log("Hi coyote, let's have some JavaScript fun!")
+
+// Goal: Create a social website for sports coaches and athletes.
+
+// The website should have the following features:
+
+// Each user has a username and password.
+// Each user is either a coach or an athlete.
+// Each user has a profile with a name, profile picture, bio and personal records.
+// Each user could tracing their training progress.
+// Coaches should pay a monthly fee to use the website.
+// Monthly fee is changed based on the number of athletes that are signed up for the coach.
+// Athletes can attend events and teams
+
+
+// Coaches homepage should have the following features:
+// Coaches profile has a list of athletes that are signed up for the coach.
+// Coaches profile has a list of training programs that are created by the coach.
+// Coaches can sell events and training programs to athletes.
+// Coaches can also create personal training programs for athletes.
+// Coaches can also create teams and athletes can sign up for them.
+// Coaches can also create posts and athletes can comment on them.
+// Coaches can also create messages and athletes can reply to them.
+
+// Athletes homepage should have the following features:
+// Athletes profile has a list of coaches that the athlete is signed up for.
+// Athletes profile has a list of teams, events and training programs that the athlete has signed up for.
+// A
+// Athletes can create posts and coaches can comment on them.
+// Athletes can also create messages and coaches can reply to them.
+// Athletes can also vote for the best coach.
+// Athletes can also buy training programs from coaches.
+
+const user = require('./user')
+
+console.log('hello world')
