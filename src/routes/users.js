c53f198a4c5ffad0e8cff1f1b53a1163f19da84f
Emre ARSLAN <112476975+emrearrslan@users.noreply.github.com>
2023-08-08 15:04:50 +0000

    Create subscriptions router

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,13 +4,24 @@ const User = require('../user.js')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
+  res.send(User.list)
 })
 
 // create a new user
 router.post('/', function (req, res, next) {
-  const newUser = new User('Emre', 'athlete', 27, 80, 180)
+  const newUser = User.create({
+    name: req.body.name,
+    status: req.body.status,
+    age: req.body.age,
+    weight: req.body.weight,
+    height: req.body.height,
+  })
   res.send(newUser)
 })
-
+router.post('/subscriptions', function (req, res, next) {
+  const athlete = User.list.find(user => user.name === req.body.athlete)
+  const coach = User.list.find(user => user.name === req.body.coach)
+  athlete.subscribe(coach)
+  res.send(athlete)
+})
 module.exports = router
